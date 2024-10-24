6b376a11c1ffd4cd8321be72d0f2fe67f775b99e
Oliver Lietz <olli@apache.org>
2024-10-24 09:50:47 +0200

    SLING-12457 Make creation of Lucene index definition configurable
    * refine test configuration
    * use non-default index name

--- src/test/java/org/apache/sling/jcr/oak/server/it/LuceneIndexIT.java
+++ src/test/java/org/apache/sling/jcr/oak/server/it/LuceneIndexIT.java
@@ -18,19 +18,9 @@
  */
 package org.apache.sling.jcr.oak.server.it;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.ops4j.pax.exam.CoreOptions.composite;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.cm.ConfigurationAdminOptions.newConfiguration;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.inject.Inject;
 
 import org.apache.jackrabbit.oak.plugins.index.IndexPathService;
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
@@ -39,39 +29,38 @@ import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.exam.cm.ConfigurationAdminOptions.newConfiguration;
+
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
 public class LuceneIndexIT extends OakServerTestSupport {
-    
+
     @Inject
     private IndexPathService indexPathService;
 
     @Override
     @Configuration
     public Option[] configuration() {
-        
-        List<Option> configs =  new ArrayList<>();
-        for ( Option option : super.configuration()) {
-            configs.add(option);
-        }
-        
         // we should only add oak-lucene but oak-lucene has a hard dependency on oak-store-document (OAK-7263)
-        
-        configs.add(composite(
-            mavenBundle("org.apache.jackrabbit", "oak-lucene", OAK_VERSION),
-            mavenBundle("org.apache.jackrabbit", "oak-store-document", OAK_VERSION),
+        return combine(
+            super.configuration(),
+            mavenBundle().groupId("org.apache.jackrabbit").artifactId("oak-lucene").version(OAK_VERSION),
+            mavenBundle().groupId("org.apache.jackrabbit").artifactId("oak-store-document").version(OAK_VERSION),
             newConfiguration("org.apache.sling.jcr.oak.server.internal.index.LuceneIndexRepositoryInitializer")
-                .put("name", "lucene")
+                .put("name", "lucy")
                 .asOption()
-          )
         );
-            
-        return configs.toArray(new Option[configs.size()]);
     }
-    
+
     @Test
-    public void ensureLuceneIndexIsCreated()  {
+    public void ensureLuceneIndexIsCreated() {
         assertThat(indexPathService, notNullValue());
-        assertThat(indexPathService.getIndexPaths(), CoreMatchers.hasItem("/oak:index/lucene"));
+        assertThat(indexPathService.getIndexPaths(), hasItem("/oak:index/lucy"));
     }
+
 }
